df928fc110e1f7309f16b1dafd098a34e208ecd9
vishnuinthad <vishnuiaviiv@gmail.com>
2019-07-26 22:37:09 +0530

    second commit

--- scrap.py
+++ scrap.py
@@ -2,16 +2,28 @@ import requests
 from bs4 import beautifulsoup
 import json
 
-cleanhouses = []
+fungame = []
 
-url="https://en.wikipedia.org/wiki/Main_Page"
+url="https://www.miniclip.com/games/en/"
 
 page=requests.get(url).content
 soup=beautifulsoup(page,'html.parser')
-houses=soup.find_all('div',class_='thumbinner mp-thumb')
-print (houses[0].prettify())
+games=soup.find_all('article',class_='slick-slide')
+
+for game in games:
+    if 'Play_code' in game['class']:
+        continue
+
+
+picture=game.find('img').findAll(itemprop="image")
+
+mygame=('picture':picture)
+
+fungme.append(mygame)
+
+#print (games[0].prettify())
 
 
 
 with open('data.json','w')as outfile:
-    json.dump(cleanhouse.outfile)
+    json.dump(fungame.outfile)
